0c54a9c68d12269aca81df29f5c56fe5ce6ce5e3
Juan Gutierrez <gutierr3z@gmail.com>
2020-12-28 13:48:26 -0500

    added main section elements

--- index.php
+++ index.php
@@ -5,12 +5,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Chalet</title>
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
     
     <main>
-        hello
+        <!-- ==================== -->
+        <section id="hero">
+            <div>
+                hero
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                overview
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section id="zillow-gallery">
+            <div>
+                zillow gallery
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section id="video">
+            <div>
+                video
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section id="map">
+            <div>
+                map
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section id="property">
+            <div>
+                property
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section id="greenhouse">
+            <div>
+                greenhouse
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Pre-Qualification / Pre-Approval</h2>
+                <p>better.com</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Online Listing</h2>
+                <p><a href="https://www.zillow.com/homes/206-Ward-Ln-Schoharie,-NY,-12157_rb/32526798_zpid/">zillow.com listing</a></p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Realtor</h2>
+                <p>1st visit</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Offer to Owners</h2>
+                <p>realtor crafts the offer.  realtor communicates with seller's agent.</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Lawyers</h2>
+                <p>after owners accept offer, offer is sent to lawyers to review. no property disclosure from owners so we get $500 credit at closing.</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Bank</h2>
+                <p>locking in loan rate of 2.75</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Inspections</h2>
+                <p>2nd visit</p>
+            </div>
+        </section>
+        <!-- ==================== -->
+        <section>
+            <div>
+                <h2>Bank's Appraisal</h2>
+                <p>hello</p>
+            </div>
+        </section>
+        <!-- ==================== -->
     </main>
 
     <script src="js/main.js"></script>
